34081eb2f486b35ea57b2dda2cd25a72e3cf4a39
Tifinity <1139390530@qq.com>
2020-01-28 09:48:47 +0800

    pointer and errors

---
+++ pointer_error/wallet.go
@@ -0,0 +1,39 @@
+package wallet
+
+import (
+	"fmt"
+	"errors"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+    String() string
+}
+
+func (b Bitcoin) String() string {
+    return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+        return InsufficientFundsError
+    }
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+    return w.balance
+}
---
+++ pointer_error/wallet_test.go
@@ -0,0 +1,52 @@
+package wallet
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+    assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+        got := wallet.Balance()
+        if got != want {
+            t.Errorf("got %s want %s", got, want)
+        }
+    }
+
+    assertError := func(t *testing.T, got error, want error) {
+	    if got == nil {
+	        t.Fatal("didn't get an error but wanted one")
+	    }
+
+	    if got != want {
+	        t.Errorf("got '%s', want '%s'", got, want)
+	    }
+	}
+
+	assertNoError := func(t *testing.T, got error) {
+	    if got != nil {
+	        t.Fatal("got an error but didnt want one")
+	    }
+	}
+
+    t.Run("Deposit", func(t *testing.T) {
+        wallet := Wallet{}
+        wallet.Deposit(Bitcoin(10))
+        assertBalance(t, wallet, Bitcoin(10))
+    })
+
+    t.Run("Withdraw", func(t *testing.T) {
+        wallet := Wallet{balance: Bitcoin(20)}
+        err := wallet.Withdraw(Bitcoin(10))
+        assertBalance(t, wallet, Bitcoin(10))
+        assertNoError(t, err)
+    })
+
+    t.Run("Withdraw insufficient funds", func(t *testing.T) { 
+	    wallet := Wallet{Bitcoin(20)}
+	    err := wallet.Withdraw(Bitcoin(100))
+	    assertBalance(t, wallet, Bitcoin(20))
+	    assertError(t, err, InsufficientFundsError)
+	})
+
+}
